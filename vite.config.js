be8b2d3d0704427f0d20830e02e5ec72f4336bbc
Joseph Rodriguez Roman <jrodriguez081192@gmail.com>
2026-03-17 23:19:40 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,13 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+                'resources/js/pages/room-show.js',
+                'resources/js/pages/room-teacher.js',
+                'resources/js/pages/room-print.js',
+            ],
             refresh: true,
         }),
         tailwindcss(),
